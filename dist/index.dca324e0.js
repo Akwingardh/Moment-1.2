651173e9c861ad533a1b658e9bc9a4d7d597fbb3
Akwingardh <Akwingardh@gmail.com>
2024-01-30 10:48:08 +0100

    Build

---
+++ dist/index.dca324e0.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("hamburger"),t=document.getElementById("list-div");e.addEventListener("click",function(){e.style.display="none",t.style.display="block"});
+//# sourceMappingURL=index.dca324e0.js.map
